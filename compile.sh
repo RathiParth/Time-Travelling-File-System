89987b25e10e79990bbd8d30c776f299739faf52
PARTH ARVIND RATHI <cs5240875@iitd.ac.in>
2025-09-11 01:54:20 +0530

    .sh

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A script to compile the Time-Travelling File System project.
+# It compiles all necessary.cpp files and links them into a single executable.
+
+echo "Compiling the Time-Travelling File System..."
+
+g++ -std=c++17 -Wall -o filesystem main.cpp Core/File.cpp Core/FileSystem.cpp
+
+echo "Compilation finished. Executable 'filesystem' created."
