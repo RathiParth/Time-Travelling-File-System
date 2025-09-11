b31346a25eee61b46f8cfe9b1aa1304858a132c3
PARTH ARVIND RATHI <cs5240875@iitd.ac.in>
2025-09-11 22:51:17 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-# Exit immediately if a command fails
-set -e
+set -e # Exit immediately if a command exits with a non-zero status.
 
 echo "Compiling the Time-Travelling File System..."
 
-g++ -std=c++17 -Wall \
-    main.cpp \
-    File/FileSystem.cpp \
-    -o filesystem
+g++ -std=c++17 -Wall main.cpp File/FileSystem.cpp File/File.cpp -o filesystem
 
 echo "Compilation finished. Executable 'filesystem' created."
+echo "You can run the program using ./filesystem"
